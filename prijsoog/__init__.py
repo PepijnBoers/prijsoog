c57cf95e87efe5ce1d791434908459255c36f6d8
Pepijn Boers <pepijnbb@gmail.com>
2021-10-10 20:17:04 +0200

    Bump version: 0.1.0 → 0.1.1

--- prijsoog/__init__.py
+++ prijsoog/__init__.py
@@ -3,5 +3,5 @@ from prijsoog.supermarkets.jumbo import JumboWatcher
 
 __author__ = """Pepijn Boers"""
 __email__ = "pepijnbb@gmail.com"
-__version__ = "0.1.0"
+__version__ = "__version__ = '0.1.1'"
 __all__ = ["AhWatcher", "JumboWatcher"]
