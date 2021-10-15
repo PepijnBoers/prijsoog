c84c958eaa04e1d604a4605f1f2f5ed988e891fa
Pepijn Boers <pepijnbb@gmail.com>
2021-10-15 17:37:05 +0200

    Fix pypi issues

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     install_requires=requirements,
     license="MIT license",
     long_description=readme + '\n\n' + history,
-    long_description_content_type='text/markdown',
+    long_description_content_type='text/x-rst',
     include_package_data=True,
     keywords='prijsoog',
     name='prijsoog',
